a2ac36fd5d12aee33505db2224fb2a5467e591b0
sabakoch <80507851+sabakoch@users.noreply.github.com>
2021-11-27 15:46:02 +0400

    finall commit

--- js.js
+++ js.js
@@ -1,62 +1,84 @@
 
 var dayBTN = document.getElementById("dayBTN")
 
+
 function day() {
+    // i am caling all qwerySelectors withe class name "hours" and changing text/ tet has changed to the last day ime track
+
+    //chaingin button color for user can see withe one is active
     document.getElementById("dayBTN").style.color = "white";
     document.getElementById("weekBTN").style.color = "hsl(236, 100%, 87%)";
     document.getElementById("monthBTN").style.color = "hsl(236, 100%, 87%)";
 
-    document.getElementById("workHr").innerHTML     = "5hrs";
-    document.getElementById("playHr").innerHTML     = "1hr";
-    document.getElementById("studyHr").innerHTML    = "0hr";
-    document.getElementById("exerciseHr").innerHTML = "1hr";
-    document.getElementById("socialHr").innerHTML   = "1hr";
-    document.getElementById("selfHr").innerHTML     = "0hr";
+    var hours = document.querySelectorAll(".hours");
+    //changig time to the day
+    hours[0].innerHTML = "5hrs";
+    hours[1].innerHTML = "1hr";
+    hours[2].innerHTML = "0hr";
+    hours[3].innerHTML = "1hr";
+    hours[4].innerHTML = "1hr";
+    hours[5].innerHTML = "0hr";
 
-    document.getElementById("timeWork").innerHTML     = "Last Day - 7hrs";
-    document.getElementById("timePlay").innerHTML     = "Last Day - 2hrs";
-    document.getElementById("timeStudy").innerHTML    = "Last Day - 1hr";
-    document.getElementById("timeExercise").innerHTML = "Last Day - 1hr";
-    document.getElementById("timeSocial").innerHTML   = "Last Day - 3hrs";
-    document.getElementById("timeSelf").innerHTML     = "Last Day - 1hr";
+    var pastTime = document.querySelectorAll(".past-time");
+    //changing time to the past day
+    pastTime[0].innerHTML = "Last Day - 7hrs";
+    pastTime[1].innerHTML = "Last Day - 2hrs";
+    pastTime[2].innerHTML = "Last Day - 1hr";
+    pastTime[3].innerHTML = "Last Day - 1hr";
+    pastTime[4].innerHTML = "Last Day - 3hrs";
+    pastTime[5].innerHTML = "Last Day - 1hr";
 }
 
 function week(){
+    // i am caling all qwerySelectors withe class name "hours" and changing text/ tet has changed to the last week ime track
+
+    //chaingin button color for user can see withe one is active
     document.getElementById("dayBTN").style.color = "hsl(236, 100%, 87%)";
     document.getElementById("weekBTN").style.color = "white";
     document.getElementById("monthBTN").style.color = "hsl(236, 100%, 87%)";
 
-    document.getElementById("workHr").innerHTML     = "32hrs";
-    document.getElementById("playHr").innerHTML     = "10hrs";
-    document.getElementById("studyHr").innerHTML    = "4hrs";
-    document.getElementById("exerciseHr").innerHTML = "4hrs";
-    document.getElementById("socialHr").innerHTML   = "5hrs";
-    document.getElementById("selfHr").innerHTML     = "2hrs";
+    var hours = document.querySelectorAll(".hours");
+    //changing time to the week
+    hours[0].innerHTML = "32hrs";
+    hours[1].innerHTML = "10hrs";
+    hours[2].innerHTML = "4hrs";
+    hours[3].innerHTML = "4hrs";
+    hours[4].innerHTML = "5hrs";
+    hours[5].innerHTML = "2hrs";
 
-    document.getElementById("timeWork").innerHTML     = "Last Day - 36hrs";
-    document.getElementById("timePlay").innerHTML     = "Last Day - 8hrs";
-    document.getElementById("timeStudy").innerHTML    = "Last Day - 7hrs";
-    document.getElementById("timeExercise").innerHTML = "Last Day - 5hrs";
-    document.getElementById("timeSocial").innerHTML   = "Last Day - 10hrs";
-    document.getElementById("timeSelf").innerHTML     = "Last Day - 2hrs";
+    var pastTime = document.querySelectorAll(".past-time");
+    //changing time to the past week 
+    pastTime[0].innerHTML = "Last Day - 36hrs";
+    pastTime[1].innerHTML = "Last Day - 8hrs";
+    pastTime[2].innerHTML = "Last Day - 7hrs";
+    pastTime[3].innerHTML = "Last Day - 5hrs";
+    pastTime[4].innerHTML = "Last Day - 10hrs";
+    pastTime[5].innerHTML = "Last Day - 2hrs";
 }
 
 function month(){
+    // i am caling all qwerySelectors withe class name "hours" and changing text/ tet has changed to the last month ime track
+    
+    //chaingin button color for user can see withe one is active
     document.getElementById("dayBTN").style.color = "hsl(236, 100%, 87%)";
     document.getElementById("weekBTN").style.color = "hsl(236, 100%, 87%)";
     document.getElementById("monthBTN").style.color = "white";
 
-    document.getElementById("workHr").innerHTML     = "103hrs";
-    document.getElementById("playHr").innerHTML     = "23hrs";
-    document.getElementById("studyHr").innerHTML    = "13hrs";
-    document.getElementById("exerciseHr").innerHTML = "11hrs";
-    document.getElementById("socialHr").innerHTML   = "21hrs";
-    document.getElementById("selfHr").innerHTML     = "7hrs";
+    var hours = document.querySelectorAll(".hours");
+    //changing times to lats month
+    hours[0].innerHTML = "103hrs";
+    hours[1].innerHTML = "23hrs";
+    hours[2].innerHTML = "13hrs";
+    hours[3].innerHTML = "11hrs";
+    hours[4].innerHTML = "21hrs";
+    hours[5].innerHTML = "7hrs";
 
-    document.getElementById("timeWork").innerHTML     = "Last Day - 128hrs";
-    document.getElementById("timePlay").innerHTML     = "Last Day - 29hrs";
-    document.getElementById("timeStudy").innerHTML    = "Last Day - 19hrs";
-    document.getElementById("timeExercise").innerHTML = "Last Day - 18hrs";
-    document.getElementById("timeSocial").innerHTML   = "Last Day - 23hrs";
-    document.getElementById("timeSelf").innerHTML     = "Last Day - 11hrs";
+    var pastTime = document.querySelectorAll(".past-time");
+    //changing timest to past month
+    pastTime[0].innerHTML = "Last Day - 128hrs";
+    pastTime[1].innerHTML = "Last Day - 29hrs";
+    pastTime[2].innerHTML = "Last Day - 19hrs";
+    pastTime[3].innerHTML = "Last Day - 18hrs";
+    pastTime[4].innerHTML = "Last Day - 23hrs";
+    pastTime[5].innerHTML = "Last Day - 11hrs";
 }
